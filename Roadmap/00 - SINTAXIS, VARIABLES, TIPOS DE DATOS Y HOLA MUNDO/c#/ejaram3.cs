952a46d0938983a368fce386384490aa5720a85a
Elkin Jaramillo <ejaram3@outlook.com>
2025-03-16 15:56:31 -0500

    #00 - c#

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c#/ejaram3.cs
@@ -0,0 +1,26 @@
+// Comentario de una línea
+// Sitio oficial: https://dotnet.microsoft.com/es-es/languages/csharp
+
+/*
+Comentarios
+varias líneas
+*/
+
+// Declaración de variables
+byte entero0a255 = 5;
+sbyte enteroConSigno127 = -5;
+short enteroPequeño = 5000;
+int entero = 5;
+long enteroLargo = 5_000_000;
+float flotante = 5000.0f;
+double doble = 5.0;
+decimal decimalv = 5.0M;
+string hola = "Hola";
+char caracter = 'A';
+bool booleano = true;
+
+// Declaración de constante
+const int constante = 5;
+
+// Imprimir por terminal
+Console.WriteLine("¡Hola, C#!");

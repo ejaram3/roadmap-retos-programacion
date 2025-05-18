6bdfcf349cb2ed78f7ba2321cfb81c3be2d01054
ejaram3 <ejaram3@outlook.com>
2025-05-18 13:35:36 -0500

    adelanto reto 01

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/ejaram3.py
@@ -0,0 +1,36 @@
+#  Arithmetic operators
+x, y = 10, 3
+
+suma = x + y
+resta = x - y
+multiplicacion = x * y
+division = x / y
+modulo = x % y
+exponenciacion = x**y
+division_piso = x // y
+
+
+# Assignment operators
+x, y = 3, 4
+
+x = 5  # asignación
+x += 3  #  asignación suma incremento
+x -= 3  #  asignación resta decremento
+x *= 3  #  asignación con multiplicación
+x /= 3  #  asignación con división
+x %= 3  #  asignación con modulo
+x **= 3  #  asignación con potencia
+x = 5
+x &= 3  #  asignación con and  bit a bit
+x = 5
+x |= 3  #  asignación con or  bit a bit
+x ^= 3 # asignación con xor bit a bit
+x >>= 3 # asignación con desplazamiento a la derecha
+
+print(x)
+
+# Comparison operators
+# Logical operators
+# Identity operators
+# Membership operators
+# Bitwise operators
